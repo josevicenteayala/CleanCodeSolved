9ac033608c41ffdde9a78e3413ae0b5bd506a277
José Vicente Ayala Luna <vin001@gmail.com>
2022-01-18 12:17:53 -0500

    Add Completable Futures examples

---
+++ CleanCodeSolvedProject/src/com/intergrupo/cleancode/complexity/IdentifyComplexity.java
@@ -0,0 +1,22 @@
+package com.intergrupo.cleancode.complexity;
+
+import java.util.concurrent.CompletableFuture;
+
+public class IdentifyComplexity {
+
+    public static void complexity (int limit) {
+        for (int i = 0; i <= limit; i++) System.out.println(i);
+    }
+
+    public static void complexity2 (int limit) {
+        for (int i = 0; i <= limit; i++) {
+            for (int j = 0; j <= limit; j++)
+            System.out.println(i*j);
+        }
+
+        for (int k = 0; k <= limit; k++) {
+            System.out.println(k*100);
+        }
+    }
+
+}
