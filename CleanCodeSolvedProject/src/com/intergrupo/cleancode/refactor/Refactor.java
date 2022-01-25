404635bb86d34185c9f017acc0c52253d9f8ed92
Jose Vicente Ayala Luna <jayala@starbucks.com>
2022-01-25 14:47:20 -0500

    Add Refactor and other clasess

---
+++ CleanCodeSolvedProject/src/com/intergrupo/cleancode/refactor/Refactor.java
@@ -0,0 +1,20 @@
+package com.intergrupo.cleancode.refactor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Refactor {
+
+    public List<UserDto> createPerson (Set<UserEntity> resultSet) {
+        List<UserDto> result = new ArrayList<>();
+        for (UserEntity e : resultSet) {
+            UserDto dto = new UserDto();
+            dto.setName (e.getName());
+            dto.setGender (e.getGender());
+            dto.setAge (e.getAge());
+            result.add(dto);
+        }
+        return result;
+    }
+}
