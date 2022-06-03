24de324822a6d6919c0fef0103892d4c908890d2
Jose Vicente Ayala Luna <jayala@starbucks.com>
2022-06-03 06:43:55 -0500

    Add Odd march bits example

---
+++ CleanCodeSolvedProject/src/com/cleancode/datastructures/ConcurrentHashMapReview.java
@@ -0,0 +1,11 @@
+package com.cleancode.datastructures;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapReview {
+    public static void main(String[] args) {
+        Map<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+    }
+
+}
